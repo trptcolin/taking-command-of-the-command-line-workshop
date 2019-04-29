5f5e97e2d07f196eedb3c4ce493e732b5ad48c86
Colin Jones <trptcolin@gmail.com>
2019-04-29 14:51:34 -0500

    Add generated docs, using grip
    See https://github.com/joeyespo/grip

---
+++ bin/generate_docs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+mkdir -p doc/generated/exercises
+
+# NOTE: uses https://github.com/joeyespo/grip
+
+find doc -name "*.md" -depth 1 -exec bash -c 'grip "$0" --export doc/generated/$(basename $0 .md).html' {} \;
+find doc/exercises -name "*.md" -depth 1 -exec bash -c 'grip "$0" --export doc/generated/exercises/$(basename $0 .md).html' {} \;
