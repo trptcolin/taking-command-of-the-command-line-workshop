1658c169cd2a65874d1ffaa15232ef1fb0739608
Colin Jones <trptcolin@gmail.com>
2019-04-30 08:03:18 -0500

    Remove "- Grip" branding on HTML title tag

--- bin/generate_docs.sh
+++ bin/generate_docs.sh
@@ -7,5 +7,5 @@ mkdir -p doc/generated/exercises
 
 # NOTE: uses https://github.com/joeyespo/grip
 
-find doc -name "*.md" -depth 1 -exec bash -c 'grip "$0" --export doc/generated/$(basename $0 .md).html' {} \;
-find doc/exercises -name "*.md" -depth 1 -exec bash -c 'grip "$0" --export doc/generated/exercises/$(basename $0 .md).html' {} \;
+find doc -name "*.md" -depth 1 -exec bash -c 'grip "$0" --export doc/generated/$(basename $0 .md).html --title="$(basename $0)"' {} \;
+find doc/exercises -name "*.md" -depth 1 -exec bash -c 'grip "$0" --export doc/generated/exercises/$(basename $0 .md).html --title="$(basename $0)"' {} \;
